3af7bb42f13c7d121a2c7a58e7ce0b14e9c4be00
Liam Speakman <ls173818@ohio.edu>
2023-12-22 13:18:51 -0500

    implemented serialization/deserialization of integers in message_format

--- message_format.h
+++ message_format.h
@@ -33,16 +33,9 @@ char* serialize(packet &msg)
 {
     // Allocate 1024 bytes
     char* bytes = new char[1024];
-    // Put in the type var first
-    bytes[0] = msg.type & 0xFF;
-    bytes[1] = (msg.type >> 8) & 0xFF;
-    bytes[2] = (msg.type >> 16) & 0xFF;
-    bytes[3] = (msg.type >> 24) & 0xFF;
-
-    bytes[4] = msg.from & 0xFF;
-    bytes[5] = (msg.from >> 8) & 0xFF;
-    bytes[6] = (msg.from >> 16) & 0xFF;
-    bytes[7] = (msg.from >> 24) & 0xFF;
+    // Add type and from information
+    serialize_int(bytes, msg.type);
+    serialize_int(&bytes[4], msg.from);
 
     // Fill out remaining bytes as message
     for(int i = 8;i<MESSAGE_LEN+8;i++)
@@ -57,9 +50,9 @@ packet* deserialize(char* bytes)
 {
     packet* msg_packet = new packet();
     msg_packet->message = new char[MESSAGE_LEN];
-    msg_packet->type = (unsigned int)(0x000000FF & bytes[0]) | (0x0000FF00 & (bytes[1] << 8)) | (0x00FF0000 & (bytes[2] << 16)) | (0xFF000000 & (bytes[3] << 24));
-    msg_packet->from = (unsigned int)(0x000000FF & bytes[4]) | (0x0000FF00 & (bytes[5] << 8)) | (0x00FF0000 & (bytes[6] << 16)) | (0xFF000000 & (bytes[7] << 24));
-
+    msg_packet->type = deserialize_int(bytes);
+    msg_packet->from = deserialize_int(&bytes[4]);
+ 
     // Fill out remaining bytes as message
     for(int i = 8;i<MESSAGE_LEN+8;i++)
     {
